3e23625a82df3924b0cc48e598f4af1e60bd7c9c
Lide <lin.lide@gmail.com>
2024-11-24 21:24:05 +0800

    day2 任務

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Page: 首頁
+  <div >
+    <h1>Page:index</h1>
+    <h2>Hi user</h2>
+    <button type="button" class="btn btn-primary">Primary</button>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+h1 {
+  color: rgb(255, 0, 0);
+  font-size: $fs-xl;
+}
+h2 {
+  color: rgb(0, 0, 255);
+  font-size: $fs-lg;
+}
+div {
+  background-color: $color_1;
+}
+</style>
