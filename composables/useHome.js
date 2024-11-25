9b4b36eb548f9adc55703a792b1be253329bdaad
Lide <lin.lide@gmail.com>
2024-11-25 22:06:48 +0800

    新增新聞資料擷取功能，重構 useHome 組件以使用 axios 並在首頁加載新聞

--- composables/useHome.js
+++ composables/useHome.js
@@ -1,3 +1,21 @@
+import { ref } from 'vue';
+import axios from "axios";
+
 export const useHome = () => {
-  return {};
+  const newsList = ref([]);
+  const isLoading = ref(false);
+
+  const getNews = async () => {
+    isLoading.value = true;
+    try {
+      const response = await axios.get('https://nuxr3.zeabur.app/api/v1/home/news/');
+      newsList.value = response.data.result;
+    } catch (error) {
+      console.error('Error fetching news:', error);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  return { newsList, isLoading, getNews };
 };
--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,11 @@
 import Loading from "vue-loading-overlay";
 import "vue-loading-overlay/dist/css/index.css";
 
-const newsList = ref([]);
-const isLoading = ref(false);
-
-// API 路徑 : https://nuxr3.zeabur.app/api/v1/home/news/
-// 使用 ES6 fetch() 或是 axios.get() 串接 API
-// 切換 isLoading 狀態
-import axios from 'axios';
+const { newsList, isLoading, getNews } = useHome();
+console.log('isLoading', isLoading.value);
 
 const fetchNews = async () => {
-  isLoading.value = true;
-  try {
-    const response = await axios.get('https://nuxr3.zeabur.app/api/v1/home/news/');
-    newsList.value = response.data.result;
-  } catch (error) {
-    console.error('Error fetching news:', error);
-  } finally {
-    isLoading.value = false;
-  }
+  await getNews();
 };
 
 onMounted(() => {
